1763b14f71efdfc36d31f39c24e256c4e4c4835c
natfresco-DK <natfresco@gmail.com>
2026-03-18 15:04:03 +0100

    Initial commit

---
+++ src/test/java/ek/dk/countries/CountriesApplicationTests.java
@@ -0,0 +1,13 @@
+package ek.dk.countries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
